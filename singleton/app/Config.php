c90837b930bb6437c285fd53382344c919850ffd
mclair52 <mathieuc52700@gmail.com>
2025-03-15 23:09:19 +0100

    add singleton

--- singleton/app/Config.php
+++ singleton/app/Config.php
@@ -3,3 +3,28 @@
 # TODO: Créer une classe Config en Singleton
 
 namespace App;
+final class Config
+{
+    private static $instance = null;
+    private $config = null;
+
+    private function __construct()
+    {
+        $this->config = require('../config/config.php');
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Config();
+        }
+        return self::$instance;
+    }
+
+    public function get($key)
+    {
+        return $this->config[$key];
+    }
+}
+
+namespace App;
--- singleton/public/index.php
+++ singleton/public/index.php
@@ -1,6 +1,14 @@
 <?php
 require('../vendor/autoload.php');
 
+use App\Config;
+
+$config1 = Config::getInstance();
+echo $config1->get('apiKey') . PHP_EOL;
+$config2 = Config::getInstance();
+
+var_dump($config1 === $config2);
+
 
 # TODO: Récuperer une instance de Config
 # Afficher une valeur contenu dans config.php
